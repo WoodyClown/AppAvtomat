b1f13e1ed3c9178680045e631147b603d1f8fd9f
WoodyClown <avdeev200514@gmail.com>
2023-05-05 14:29:33 +0300

    Добавьте файлы проекта.

---
+++ AppAvtomat/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace AppAvtomat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rowsTextBox = new System.Windows.Forms.TextBox();
+            this.colsTextBox = new System.Windows.Forms.TextBox();
+            this.createTableButton = new System.Windows.Forms.Button();
+            this.labeltest1 = new System.Windows.Forms.Label();
+            this.labeltest2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rowsTextBox
+            // 
+            this.rowsTextBox.Location = new System.Drawing.Point(218, 176);
+            this.rowsTextBox.Name = "rowsTextBox";
+            this.rowsTextBox.Size = new System.Drawing.Size(155, 22);
+            this.rowsTextBox.TabIndex = 0;
+            // 
+            // colsTextBox
+            // 
+            this.colsTextBox.Location = new System.Drawing.Point(379, 176);
+            this.colsTextBox.Name = "colsTextBox";
+            this.colsTextBox.Size = new System.Drawing.Size(147, 22);
+            this.colsTextBox.TabIndex = 1;
+            // 
+            // createTableButton
+            // 
+            this.createTableButton.Location = new System.Drawing.Point(327, 204);
+            this.createTableButton.Name = "createTableButton";
+            this.createTableButton.Size = new System.Drawing.Size(90, 23);
+            this.createTableButton.TabIndex = 2;
+            this.createTableButton.Text = "Создать";
+            this.createTableButton.UseVisualStyleBackColor = true;
+            this.createTableButton.Click += new System.EventHandler(this.createTableButton_Click);
+            // 
+            // labeltest1
+            // 
+            this.labeltest1.AutoSize = true;
+            this.labeltest1.Location = new System.Drawing.Point(269, 157);
+            this.labeltest1.Name = "labeltest1";
+            this.labeltest1.Size = new System.Drawing.Size(54, 16);
+            this.labeltest1.TabIndex = 3;
+            this.labeltest1.Text = "Строки";
+            // 
+            // labeltest2
+            // 
+            this.labeltest2.AutoSize = true;
+            this.labeltest2.Location = new System.Drawing.Point(418, 157);
+            this.labeltest2.Name = "labeltest2";
+            this.labeltest2.Size = new System.Drawing.Size(64, 16);
+            this.labeltest2.TabIndex = 4;
+            this.labeltest2.Text = "Столбцы";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labeltest2);
+            this.Controls.Add(this.labeltest1);
+            this.Controls.Add(this.createTableButton);
+            this.Controls.Add(this.colsTextBox);
+            this.Controls.Add(this.rowsTextBox);
+            this.Name = "Form1";
+            this.Text = "Задание на автомат";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox rowsTextBox;
+        private System.Windows.Forms.TextBox colsTextBox;
+        private System.Windows.Forms.Button createTableButton;
+        private System.Windows.Forms.Label labeltest1;
+        private System.Windows.Forms.Label labeltest2;
+    }
+}
+
---
+++ AppAvtomat/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppAvtomat
+{
+    public partial class Form1 : Form
+    {
+        private TableLayoutPanel table;
+        private Button calculateButton;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void createTableButton_Click(object sender, EventArgs e)
+        {
+            int rows = int.Parse(rowsTextBox.Text);
+            int cols = int.Parse(colsTextBox.Text);
+
+            table = new TableLayoutPanel();
+            rowsTextBox.Visible = false;
+            colsTextBox.Visible = false;
+            labeltest1.Visible = false;
+            labeltest2.Visible = false;
+            createTableButton.Visible = false;
+            table.RowCount = rows + 1; // учитываем строку заголовков
+            table.ColumnCount = cols + 3; // учитываем столбец заголовков
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F)); // задаем ширину столбцов
+            Label labelk = new Label();
+            labelk.Text = "Критерии";
+            labelk.Dock = DockStyle.Fill;
+            labelk.TextAlign = ContentAlignment.MiddleCenter;
+            table.Controls.Add(labelk, 0, 0);
+            for (int i = 1; i <= cols; i++) // добавляем заголовки столбцов
+            {
+                table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+                Label label = new Label();
+                label.Text = "L" + i.ToString();
+                label.Dock = DockStyle.Fill;
+                label.TextAlign = ContentAlignment.MiddleCenter;
+                table.Controls.Add(label, i, 0);
+            }
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            Label label1 = new Label();
+            label1.Text = "Вектор\r\nкритериев";
+            label1.Dock = DockStyle.Fill;
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            table.Controls.Add(label1, cols + 1, 0);
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            Label label2 = new Label();
+            label2.Text = "Вектор\r\nприоритетов";
+            label2.Dock = DockStyle.Fill;
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            table.Controls.Add(label2, cols + 2, 0);
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F)); // задаем высоту строки заголовков
+            for (int i = 1; i <= rows; i++) // добавляем заголовки строк
+            {
+                table.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+                Label label = new Label();
+                label.Text = "L" + i.ToString();
+                label.Dock = DockStyle.Fill;
+                label.TextAlign = ContentAlignment.MiddleCenter;
+                table.Controls.Add(label, 0, i);
+            }
+            for (int i = 1; i <= rows; i++) // добавляем ячейки таблицы
+            {
+                for (int j = 1; j <= cols; j++)
+                {
+                    TextBox textBox = new TextBox();
+                    textBox.Dock = DockStyle.Fill;
+                    textBox.TextAlign = HorizontalAlignment.Center;
+                    table.Controls.Add(textBox, j, i);
+                }
+            }
+            table.AutoScroll = true;
+            table.Dock = DockStyle.Fill;
+            this.Controls.Add(table);
+
+
+
+            calculateButton = new Button();
+            calculateButton.Text = "Посчитать";
+            calculateButton.Anchor = AnchorStyles.Right | AnchorStyles.Bottom;
+            calculateButton.Click += new EventHandler(CalculateRowsSum);
+            table.Controls.Add(calculateButton, 0, rows + 1);
+            table.AutoScroll = true;
+            table.Dock = DockStyle.Fill;
+        }
+        private void CalculateRowsSum(object sender, EventArgs e)
+        {
+            calculateButton.Enabled = false;
+            for (int i = 1; i < table.RowCount; i++)
+            {
+                double rowSum = 0;
+                // Проходим по каждому TextBox в строке
+                foreach (Control control in table.Controls)
+                {
+                    if (table.GetRow(control) == i && control is TextBox textBox)
+                    {
+                        if (double.TryParse(textBox.Text, out double value))
+                        {
+                            // Если удалось успешно распарсить число из TextBox, то добавляем его к сумме строки
+                            rowSum += value;
+                        }
+                    }
+                }
+                table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+                Label label = new Label();
+                label.Text = rowSum.ToString();
+                label.Dock = DockStyle.Fill;
+                label.TextAlign = ContentAlignment.MiddleCenter;
+                table.Controls.Add(label, table.ColumnCount - 2, i);
+                // Выводим сумму строки в консоль (вы можете сделать что-то другое с этой информацией)
+            }
+        }
+    }
+}
